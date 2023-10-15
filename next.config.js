6bd43699d765316e4b2bf82db2980a8a4399ee9d
Ismael <ismaelbetzoletti@gmail.com>
2023-10-15 16:15:42 -0300

    Project reation + Initial configuration + Doing firs page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = nextConfig;
